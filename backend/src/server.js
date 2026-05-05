8ddd89a026a0e9b76c107111bafd08d70cba5ed8
Abhishek <abhishekwx7@gmail.com>
2026-05-05 15:59:00 +0530

    Welcome email implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
+import { ENV } from "./lib/env.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config();
-
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
 
